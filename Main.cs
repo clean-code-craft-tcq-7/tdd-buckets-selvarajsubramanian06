6b5e429369a75d17b731f40dd2347d39ad9fb1da
selvarajsubramanian06 <115691011+selvarajsubramanian06@users.noreply.github.com>
2023-07-07 18:28:36 +0530

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDRange
+{
+    public class Main
+    {
+        static void Main(string[] args)
+        {
+            CurRangeTest currentRangeTest = new CurRangeTest();
+            currentRangeTest.ValidDuplicateInputs();
+            currentRangeTest.CaptureValuesCount();
+            currentRangeTest.CheckSequenceNumber();
+            currentRangeTest.CaptureWithCount();
+            currentRangeTest.CaptureRange();
+
+            A2DTest a2DConvertorTest = new A2DTest();
+            a2DConvertorTest.CreateConvertorObject();
+            a2DConvertorTest.ValidateErrorInput();
+            a2DConvertorTest.ValidateInput();
+            a2DConvertorTest.ValidateInvalidInputs();
+            a2DConvertorTest.DetectRangeIgnoringErrorValue();
+        }
+    }    
+}
