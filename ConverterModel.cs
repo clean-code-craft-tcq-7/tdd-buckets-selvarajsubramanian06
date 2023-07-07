b65faa9430a0b4ba567f2f6c8b3a23ad82d333a6
selvarajsubramanian06 <115691011+selvarajsubramanian06@users.noreply.github.com>
2023-07-07 18:14:56 +0530

    Create ConverterModel.cs

---
+++ ConverterModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Range
+{
+    public class ConverterModel
+    {
+        public decimal AmpValue;
+        public int Error;
+    }
+}
